55c9bab463fb3aa25c9945d49ad27052181f7a58
a.z.tvgg@icloud.com <a.z.tvgg@icloud.com>
2020-10-07 15:21:27 +0700

    Initial commit

---
+++ src/test/java/com/example/akedemyTodo/AkedemyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.akedemyTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkedemyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
