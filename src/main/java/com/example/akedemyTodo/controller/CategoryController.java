2f6ab5bcb376400f8d9fba6aafc7462c564b2116
a.z.tvgg@icloud.com <a.z.tvgg@icloud.com>
2020-10-13 21:25:21 +0700

    добавил метод add в категориКонтроллер и приритиКонтролер + в категорииКонтролер пошел update

--- src/main/java/com/example/akedemyTodo/controller/CategoryController.java
+++ src/main/java/com/example/akedemyTodo/controller/CategoryController.java
@@ -2,11 +2,11 @@ package com.example.akedemyTodo.controller;
 
 import com.example.akedemyTodo.entity.Category;
 import com.example.akedemyTodo.repo.CategoryRepository;
+
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -14,7 +14,7 @@ import java.util.List;
 @RequestMapping ("/category")
 public class CategoryController {
 
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
 
     public CategoryController(CategoryRepository categoryRepository) {
@@ -29,6 +29,35 @@ public class CategoryController {
 
 
         return list ;
+
+    }
+    @PostMapping("/add")
+    public ResponseEntity<Category> add(@RequestBody Category category){
+
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Category category){
+
+        if (category.getId() == null || category.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
 
     }
 
--- src/main/java/com/example/akedemyTodo/controller/PriorityController.java
+++ src/main/java/com/example/akedemyTodo/controller/PriorityController.java
@@ -3,10 +3,9 @@ package com.example.akedemyTodo.controller;
 import com.example.akedemyTodo.entity.Priority;
 import com.example.akedemyTodo.repo.PriorityRepository;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,7 +14,7 @@ import java.util.List;
 @RequestMapping ("/priority")
 public class PriorityController {
 
-    private PriorityRepository priorityRepository;
+    private final PriorityRepository priorityRepository;
 
 
     public PriorityController(PriorityRepository priorityRepository) {
@@ -32,6 +31,24 @@ public class PriorityController {
         return list;
 
     }
+    @PostMapping("/add")
+    public ResponseEntity<Priority> add(@RequestBody Priority priority){
+
+        // проверка на обязательные параметры
+        if (priority.getId() != null && priority.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
 
 
 }
